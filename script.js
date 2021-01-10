e10caedc2aea7bb8ee29ec934da0300ef804a0ff
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-09 18:06:07 -0600

    Add Textarea Below Date With Grey Background.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     var DateTime = luxon.DateTime;
     var today = DateTime.local();
     var f = { month: "long", day: "2-digit" }
+    var nine = $("#9am");
+    var x = true;
 
     $("#current-date").text(today.toLocaleString(f));
 
