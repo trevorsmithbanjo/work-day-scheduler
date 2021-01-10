83d7ce0bfb454c26831d3b95b094865cb9991675
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-10 14:27:47 -0600

    Create first entry row.

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ $(document).ready(function () {
     var f = { month: "long", day: "2-digit" }
     var nine = $("#9am");
     var x = true;
-
+    // Array of hours
+    var hoursArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+    // Current date header
     $("#current-date").text(today.toLocaleString(f));
+
+
 
 })
