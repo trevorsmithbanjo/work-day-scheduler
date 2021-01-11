1736393c584f6668d6cd041ccf36fbb89c320ad3
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-11 07:27:56 -0600

    Update Script.

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ $(document).ready(function () {
     var today = DateTime.local();
     var monthDay = { month: "long", day: "2-digit" }
     var hour = { hour: "numeric" };
-    var currentHour = today.setLocale("de").toLocaleString(hour);
+    // var currentHour = today.setLocale("de").toLocaleString(hour);
 
-    // Main container div
+    // Selectors
     var container = $(".container");
-    console.log(today.setLocale("de").toLocaleString(hour));
 
     // Array of hours
     var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
@@ -18,28 +17,37 @@ $(document).ready(function () {
     // Current date header
     $("#current-date").text(today.toLocaleString(monthDay));
 
+
     // For loop to populate entry rows
-    for (i = 0; i <= 8; i++) {
-        var entryRow = $(`<div class="row mb-2">
+    for (var i = 0; i <= 8; i++) {
+        var entryRow = $(`<div class="row mb-2" id="entry-row">
         <hr>
         <div class="col-3 text-end p-0">
             <h2>${hours[i]}</h2>
         </div>
         <div class="col-6">
-            <textarea class="bg-secondary form-contorl" id="${hours24[i]}" rows="4" style="width:100%"></textarea>
+            <textarea class="form-contorl" id="${hours24[i]}" value="${hours24[i]}" rows="4" style="width:100%"></textarea>
         </div>
         <div class="col-3 d-grid gap-2 p-0">
-            <button class="btn bg-info text-center" id="${hours[i]}">Save</button>
+            <button class="btn bg-info text-center" id="${hours24[i]}" value="${hours24[i]}">Save</button>
         </div>
     </div>`);
         container.append(entryRow);
     }
 
+    $("textarea").each(function (i, j) {
+        console.log(i);
+        console.log(j);
+    })
+
     // function to remove letters & whitespace from currentHour
-    function hrToNum(currentHour) {
-        currentHour = currentHour.replace(/\s\w+/g, "");
+    function hrToNum() {
+        var currentHour = today.setLocale("de")
+            .toLocaleString(hour)
+            .replace(/\s\w+/g, "");
         console.log("New Current Hour: " + currentHour);
         return currentHour;
     }
+    hrToNum();
 
 })
