c404ce0f756a98d36b09fd74fd11d2e153fe6148
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-11 18:50:36 -0600

    Still Need To Fix Background Color.

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ $(document).ready(function () {
     var timeBlock = $(".time-block");
     var description = $(".description");
 
-    // Array of 24 hour time
-    var hours24 = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
-
     // Current date header
     $("#currentDay").text(today.toLocaleString(monthDay));
 
@@ -30,13 +27,12 @@ $(document).ready(function () {
         hrToNum(currentHour);
         console.log(currentHour);
         for (var i = 0; i < 9; i++) {
-            console.log(description[i].id);
             if (+description[i].id > +currentHour) {
-                description.addClass("future").removeClass("past");
+                description[i].addClass("future").removeClass("past");
             } else if (+description[i].id === +currentHour) {
-                description.addClass("present").removeClass("future");
+                description[i].addClass("present").removeClass("future");
             } else {
-                description.addClass("past").removeClass("present");
+                description[i].addClass("past").removeClass("present");
             }
         }
     }
